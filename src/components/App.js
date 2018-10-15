54a74706040cfaed56d04a42869cbbb82c196549
Saalim Khan <khansaalim12@gmail.com>
2018-10-15 08:15:02 +0530

    initital

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+import '../styles/common.css'
+
+export default class App extends Component{
+    render() {
+        return(
+        <div>
+            <h2 className="heading">My React App</h2>
+        </div>
+        )
+    }
+}
