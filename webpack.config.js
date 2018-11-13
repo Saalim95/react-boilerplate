59698cca701b127d067bbe146798e0a2859df7f0
Saalim Khan <Saalim95@users.noreply.github.com>
2018-11-13 15:33:17 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,8 @@ module.exports = {
     entry: './src/index.js',
     output: {
         path: path.join(__dirname, '/dist'),
-        filename: 'index_bundle.js'
+        filename: 'index_bundle.js',
+	publicPath:env=="dev"?'/':'/dist/'
     },
     module: {
         rules: [
